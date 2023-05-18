b675758443848ebde2ca834f09341273f73a3248
hznutx <userhzn@gmail.com>
2023-05-18 17:26:44 +0700

    update

--- src/component/Randommeal.jsx
+++ src/component/Randommeal.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import randommenu from "../assets/data/menurandom.json";
-import { Grid, Button, Typography, Card } from "@mui/material";
+import { Grid, Button, Typography, Card, Box } from "@mui/material";
+import { RestaurantMenu } from "@mui/icons-material";
 
 const RandomMeal = () => {
   const [randomData, setRandomData] = useState([]);
   const [rowCount, setRowCount] = useState(0);
 
-  const getRandomResult = () => {
+  const getRandomtitle = () => {
     const randomIndex = Math.floor(Math.random() * randommenu.data.length);
     return randommenu.data[randomIndex];
   };
@@ -14,7 +15,7 @@ const RandomMeal = () => {
   const handleRandomClick = () => {
     let newState = [];
     for (let i = 0; i < rowCount; i++) {
-      newState.push(getRandomResult());
+      newState.push(getRandomtitle());
     }
     setRandomData(newState);
   };
@@ -22,12 +23,6 @@ const RandomMeal = () => {
   const handleClearData = () => {
     setRandomData([]);
   };
-
-  const handleTryAgainClick = () => {
-    const newRandomResult = getRandomResult();
-    setRandomData([newRandomResult]);
-  };
-
   const handleAdjustRows = (number) => {
     if (number === "+") {
       setRowCount((prevCount) => prevCount + 1);
@@ -37,19 +32,34 @@ const RandomMeal = () => {
   };
 
   return (
-    <Grid container gap={4} flexDirection="column">
-      <Grid item alignItems="center">
+    <Grid
+      container
+      flexDirection="column"
+      gap={2}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid
+        item
+        xs
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        mt={4}
+      >
         <Button
           variant="contained"
           style={{
             borderRadius: "25px",
-            marginRight: 10,
+            marginRight: 15,
           }}
           onClick={() => handleAdjustRows("-")}
         >
           -
         </Button>
-        <Button variant="contained">{rowCount}</Button>
+        <Button variant="contained" disabled={!rowCount}>
+          {rowCount}
+        </Button>
         <Button
           variant="contained"
           style={{
@@ -61,29 +71,33 @@ const RandomMeal = () => {
           +
         </Button>
       </Grid>
-
-      <Button
-        variant="contained"
-        style={{
-          textTransform: "capitalize",
-          height: "50px",
-          fontSize: "20px",
-          p: 20,
-          borderRadius: "25px",
-          color: "white",
-        }}
-        color="secondary"
-        disabled={!rowCount}
-        onClick={handleRandomClick}
-      >
-        {!randomData.length ? "Random" : "Try Again"}
-      </Button>
-
+      <Grid item>
+        <Button
+          variant="contained"
+          style={{
+            textTransform: "capitalize",
+            height: "50px",
+            fontSize: "20px",
+            p: 20,
+            borderRadius: "25px",
+            color: "white",
+          }}
+          color="secondary"
+          disabled={!rowCount}
+          onClick={handleRandomClick}
+        >
+          {!randomData.length ? "Random" : "Try Again"}
+        </Button>
+      </Grid>
       <Card elevation={2} sx={{ borderRadius: 10, p: 2 }}>
         {randomData.map((item, index) => (
-          <Typography key={index}>{item.result}</Typography>
+          <Typography fontSize="25px" key={index}>
+            <RestaurantMenu sx={{ mr: 1 }} />
+            {item.title}
+          </Typography>
         ))}
       </Card>
+
       <Button
         variant="contained"
         style={{
