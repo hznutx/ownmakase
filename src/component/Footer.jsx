6bd0bc05dd2dd5c249d3cdb4f9d9367d3e81022c
uneemo / <uneemo.4.work@gmail.com>
2023-05-25 11:10:36 +0700

    ^ something :

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,42 +1,34 @@
-import RestaurantMenuOutlinedIcon from "@mui/icons-material/RestaurantMenuOutlined";
-import CalculateOutlinedIcon from "@mui/icons-material/CalculateOutlined";
-import ClassOutlinedIcon from "@mui/icons-material/ClassOutlined";
-import BottomNavigation from "@mui/material/BottomNavigation";
-import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import { Paper } from "@mui/material";
-import { useState } from "react";
+import { Paper, BottomNavigation, BottomNavigationAction } from "@mui/material";
+import { RestaurantMenuOutlined, CalculateOutlined, ClassOutlined, Tune } from "@mui/icons-material";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SimpleBottomNavigation() {
   const [value, setValue] = useState(0);
+  const navigate = useNavigate()
+
+  const onNavClick = (e, newValue) => {
+    setValue(newValue);
+  }
+
+  const onMenuClick = (path) => {
+    navigate(path, { replace: true })
+  }
+
+  useEffect(() => {
+    console.log("reload . . .")
+  }, [])
+
 
   return (
     <Paper
-      sx={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-        backgroundColor: "white",
-      }}
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0, backgroundColor: "white" }}
       elevation={0}
     >
-      <BottomNavigation
-        showLabels
-        value={value}
-        sx={{ d: "" }}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Recipes" icon={<ClassOutlinedIcon />} />
-        <BottomNavigationAction
-          label="Random !"
-          icon={<RestaurantMenuOutlinedIcon />}
-        />
-        <BottomNavigationAction
-          label="Go Shopping"
-          icon={<CalculateOutlinedIcon />}
-        />
+      <BottomNavigation showLabels value={value} onChange={onNavClick} >
+        <BottomNavigationAction label="Recipes" icon={<ClassOutlined />} onClick={() => onMenuClick('menu')} />
+        <BottomNavigationAction label="Random !" icon={<RestaurantMenuOutlined onClick={() => onMenuClick('random-food')} />} />
+        <BottomNavigationAction label="Go Shopping" icon={<CalculateOutlined onClick={() => onMenuClick('shopping-list')} />} />
       </BottomNavigation>
     </Paper>
   );
